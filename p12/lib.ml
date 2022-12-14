a65bfec96d6113e2b6b5b1bcab383e4e8aa9f458
Carmelo Alejandro García Almeida <115218862+SkoS27@users.noreply.github.com>
2022-12-14 13:48:47 +0100

    Delete p12 directory

--- p12/lib.ml
+++
@@ -1,7 +0,0 @@
-
-exception Function_not_defined of string;;
-
-let funs = ... ;;
-
-let get_function s = ...  ;;
-
--- p12/lib.mli
+++
@@ -1,5 +0,0 @@
-
-exception Function_not_defined of string
-
-val get_function : string -> (float -> float)
-
