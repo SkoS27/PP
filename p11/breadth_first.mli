214b69df2b1cd1a15f870fc349e780b12160c404
Carmelo Alejandro García Almeida <115218862+SkoS27@users.noreply.github.com>
2022-12-14 13:46:31 +0100

    Delete p11 directory

--- p11/breadth_first.mli
+++
@@ -1,7 +0,0 @@
-
-val breadth_first : 'a G_tree.g_tree -> 'a list
-
-val breadth_first_t : 'a G_tree.g_tree -> 'a list
-
-val t2 : int G_tree.g_tree
-
--- p11/tsort.ml
+++
@@ -1,9 +0,0 @@
-
-open Bin_tree;;
-
-let rec insert_tree ord x = ... ;;
-
-let tsort ord l =
-  inorder (List.fold_left (fun a x -> insert_tree ord x a) Empty l)
-;;
-
