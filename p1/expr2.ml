3b8288a0a1186c92394ded6c24e5c5025e86b0e7
Carmelo Alejandro García Almeida <115218862+SkoS27@users.noreply.github.com>
2022-12-29 10:20:15 +0100

    Update expr2.ml

--- p1/expr2.ml
+++ p1/expr2.ml
@@ -2,5 +2,5 @@ let u = 5 + 2 + 2 + 3;;
 let pi = 2.0 *. asin 1.0 ;;
 let v = 2. *. pi *. 2.;;
 let w = char_of_int 57 ;;
-let x = true && false;;;
-let y = if 3 = 3 then "true" else "false" ;
+let x = true && false;;
+let y = if 3 = 3 then "true" else "false" ;;
